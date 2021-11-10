4a5a30755a968b9b51ae163dce96e22accd1a3c5
Alihan Açıkgöz <76866068+aliacikgoz91@users.noreply.github.com>
2021-11-10 23:55:11 +0300

    Add files via upload

---
+++ 203801061 - Alihan Açıkgöz - NtpOdev4/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NtpOdev4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.IsMdiContainer = true; // Form içinde form açılabilir yapma
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            Form2 form2 = new Form2(); // Form 2'yi aç
+            form2.Show(); // Form2'yi göster
+
+
+        }
+    }
+}
